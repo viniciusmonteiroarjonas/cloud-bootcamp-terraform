ac5b93e828da2fb0296f309998783180e1509dfb
vinicius arjonas <vinicius.arjonas.monteiro@gmail.com>
2023-06-23 12:43:27 -0300

    Implementado consulta agendamentos e cadastro

--- lambdas/agendamento/index.js
+++ lambdas/agendamento/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// const AWS = require('aws-sdk')
-// const dynamo = new AWS.DynamoDB.DocumentClient()
+const AWS = require('aws-sdk')
+const dynamo = new AWS.DynamoDB.DocumentClient()
 
 const normalizeEvent = (event) => {
     // Api Gateway
@@ -14,15 +14,40 @@ const normalizeEvent = (event) => {
 
 exports.handler = async (event) => {
 
-    const { method, data, querystring } = normalizeEvent(event)
-
-    const resultado = {
-        "mensagem": "Sucesso"
+    if (process.env.DEBUG) {
+        console.log(`Received event: ${JSON.stringify(event)}`)
     }
 
+    const table = "meu-guia-agendamentos";
+    if (!table) {
+        throw new Error('No table name defined.')
+    }
+
+    const { method, data, querystring } = normalizeEvent(event)
+    const params = { TableName: table }
+
+    let res;
+    let err;
+
+    try {
+        switch (method) {
+            case 'GET':
+                res = await dynamo.scan(params).promise()
+                break;
+            case 'POST':
+                res = await dynamo.put({ ...params, Item: data }).promise()
+                break;
+            default:
+                err = new Error(`Unsupported method "${method}"`)
+        }
+    } catch (error) {
+        err = new Error(error.message)
+    }
+
+
     return {
-        statusCode: 200,
-        body: JSON.stringify(resultado),
+        statusCode: err ? 500 : 200,
+        body: err ? err.message : JSON.stringify(res),
         headers: {
             'Content-Type': 'application/json',
         }

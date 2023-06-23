e0747a15826c42754470cef3261cf475862ee31a
vinicius arjonas <vinicius.arjonas.monteiro@gmail.com>
2023-06-23 15:01:10 -0300

    Implementado código terraform para criar uma usuário cognito

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -19,3 +19,15 @@ resource "aws_cognito_user_pool_domain" "this" {
   domain       = var.service_name
   user_pool_id = aws_cognito_user_pool.this.id
 }
+
+
+resource "aws_cognito_user" "this" {
+  user_pool_id          = aws_cognito_user_pool.this.id
+  username              = "cloud"
+  temporary_password    = "TempPass123!"  # Senha temporária para o usuário
+  message_action        = "SUPPRESS"      # Suprime o envio de um e-mail ao usuário com as informações da conta
+
+  depends_on = [
+    aws_cognito_user_pool_client.this
+  ]
+}
